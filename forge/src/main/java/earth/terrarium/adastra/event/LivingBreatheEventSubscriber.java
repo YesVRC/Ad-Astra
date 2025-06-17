e94823b94df38789687050e9122c8b6c323cbf66
YesVRC <altair9090@gmail.com>
2025-06-17 08:43:55 -0400

    Ticks and stuff are right

--- forge/src/main/java/earth/terrarium/adastra/event/LivingBreatheEventSubscriber.java
+++ forge/src/main/java/earth/terrarium/adastra/event/LivingBreatheEventSubscriber.java
@@ -2,11 +2,14 @@ package earth.terrarium.adastra.event;
 
 import earth.terrarium.adastra.AdAstra;
 import earth.terrarium.adastra.api.systems.OxygenApi;
+import earth.terrarium.adastra.common.handlers.PlanetHandler;
 import earth.terrarium.adastra.common.items.armor.SpaceSuitItem;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.event.entity.living.LivingBreatheEvent;
@@ -14,16 +17,17 @@ import net.minecraftforge.event.entity.living.LivingBreatheEvent;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = AdAstra.MOD_ID)
 public class LivingBreatheEventSubscriber {
 
-    @SubscribeEvent
-    public static void subLivingBreatheEvent(LivingBreatheEvent event){
+    @SubscribeEvent(priority = EventPriority.NORMAL)
+    public static void spaceSuitLivingBreatheEvent(LivingBreatheEvent event){
         if(event.getEntity() instanceof ServerPlayer player) {
             ItemStack stack = player.getItemBySlot(EquipmentSlot.CHEST);
             if(stack.getItem() instanceof SpaceSuitItem item
-                && (SpaceSuitItem.hasOxygen(player))
+                && player.tickCount % 12 == 0
+                && SpaceSuitItem.hasOxygen(player)
                 && !event.canBreathe()
                 && player.getEyeInFluidType() != ForgeMod.WATER_TYPE.get())
             {
-                if(player.tickCount % 12 == 0) item.consumeOxygen(stack, 1);
+                item.consumeOxygen(stack, 1);
                 event.setCanBreathe(true);
                 event.setCanRefillAir(true);
             }
